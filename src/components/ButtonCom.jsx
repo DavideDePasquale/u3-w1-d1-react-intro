04ad3ca03b8e48fb855142fc785ebc9edd5ef23e
Davide De Pasquale <davide.depasquale1998@gmail.com>
2024-11-25 17:42:05 +0100

    abab

--- src/components/ButtonCom.jsx
+++ src/components/ButtonCom.jsx
@@ -1,12 +1,21 @@
-const ButtonCom = props => { 
-    return (
+const ButtonCom = (props) => {
+  return (
     <>
-    <div>
-    <label htmlFor={props.id} style={props.style2}>{props.labelText}</label>
-    </div>
-    <input type={props.type} placeholder={props.placeholder} id={props.id} style={props.style3}/>
-    <button type={props.buttype} style={props.style}>{props.buttonText}</button>
-   </>
-    )
+      <div>
+        <label htmlFor={props.id} style={props.style2}>
+          {props.labelText}
+        </label>
+      </div>
+      <input
+        type={props.type}
+        placeholder={props.placeholder}
+        id={props.id}
+        style={props.style3}
+      />
+      <button type={props.buttype} style={props.style}>
+        {props.buttonText}
+      </button>
+    </>
+  );
 };
-export default ButtonCom
+export default ButtonCom;
--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,12 +1,10 @@
-const ImageComponent = props =>{ 
-    return ( 
+const ImageComponent = (props) => {
+  return (
     <>
-    <h1>{props.title}
-    </h1>
-    <p>{props.paragraph}
-    </p>
-    <img src={props.immagine} alt={props.title} style={props.style}/>
+      <h1>{props.title}</h1>
+      <p>{props.paragraph}</p>
+      <img src={props.immagine} alt={props.title} style={props.style} />
     </>
-    )
+  );
 };
-export default ImageComponent
+export default ImageComponent;
